2e8bf9c8ad8371b200fa1cbdfe268c27712b3bc9
ssydykov <saken2316@gmail.com>
2019-01-06 20:01:21 +0600

    Bug fixed

--- telegramform.js
+++ telegramform.js
@@ -1,5 +1,14 @@
+// $('#apply').on('show.bs.modal', function (event) {
+//   var button = $(event.relatedTarget) // Button that triggered the modal
+//   var recipient = button.data('type') // Extract info from data-* attributes
+//   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+//   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+//   var modal = $(this)
+//   modal.find('.modal-subtitle').text('Бесплатная консультация по курсу "' + recipient + '"')
+//   modal.find('.modal-body input').val(recipient)
+// })
 $(document).ready(function () {
-    
+
     $(".form-signin").submit(function () {
 
         var formID = $(this).attr('id');
@@ -14,10 +23,20 @@ $(document).ready(function () {
         var test = $('#username').val();
         var name = $('#username').val();
         var phone = $('#phone').val();
+        var age = $('#age').val();
+        var type = $('#type').val();
         var email = $('#email').val();
         var message = $('#message').val();
 
-        var postMessage = "Name: " + name + "%0APhone: " + phone + "%0AEmail: " + email + "%0AMessage: " + message;
+        $('#username').val('');
+        $('#phone').val('');
+        $('#age').val('');
+        $('#email').val('');
+        $('#message').val('');
+
+        $('#apply').modal('hide');
+
+        var postMessage = "Имя: " + name + "%0AТелефон: " + phone + "%0AEmail: " + email  + "%0AВозраст: " + age + "%0AMessage: " + message;
 
         // alert(postMessage);
 
